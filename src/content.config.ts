9a8e033942f720ed5617504a0ccbaba7ef253475
Chris Impicciche <chrisimpicciche@Chriss-MacBook-Pro.local>
2025-06-05 12:46:56 -0500

    update astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
   schema: ({ image }) =>
     z.object({
       title: z
@@ -17,28 +18,28 @@ const blog = defineCollection({
           'Description should be 155 characters or less for optimal Open Graph display.',
         ),
       date: z.coerce.date(),
-      image: image()
-        .refine((img) => img.width === 1200 && img.height === 630, {
-          message:
-            'The image must be exactly 1200px × 630px for Open Graph requirements.',
-        })
-        .optional(),
+      image: image().optional(),
+        // .refine((img) => img.width === 1200 && img.height === 630, {
+        //   message:
+        //     'The image must be exactly 1200px × 630px for Open Graph requirements.',
+        // })
       tags: z.array(z.string()).optional(),
       draft: z.boolean().optional(),
     }),
 })
 
 const projects = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/projects" }),
   schema: ({ image }) =>
     z.object({
       name: z.string(),
       description: z.string(),
       tags: z.array(z.string()),
-      image: image().refine((img) => img.width === 1200 && img.height === 630, {
-        message:
-          'The image must be exactly 1200px × 630px for Open Graph requirements.',
-      }),
+      image: image(),
+      // .refine((img) => img.width === 1200 && img.height === 630, {
+      //   message:
+      //     'The image must be exactly 1200px × 630px for Open Graph requirements.',
+      // }),
       link: z.string().url(),
       year: z.number().min(1990).optional(),
     }),
--- src/content/projects/2-designLab.md
+++ src/content/projects/2-designLab.md
@@ -1,6 +1,6 @@
 ---
 name: 'CustomInk Design Lab'
-description: "I've been contributing to the lab since 2022"
+description: "Design custom swag and apparel"
 tags: ['React', 'Redux Toolkit', 'Jest', 'Sass']
 image: '../../images/designLab.png'
 link: 'https://www.customink.com/ndx'
